df323b07b650bd577847dfbd1704b2b28a7ea7b0
Ricardo Carruyo <rcarruyo82@gmail.com>
2020-08-01 15:01:11 -0300

    Test primera Implementacion

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ricardo Carruyo website
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>Ricardo Carruyo</div>
+  )
+
+
 }
 
 export default App;
